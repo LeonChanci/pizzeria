b34fa00df08ac9a9ae995c4129ce26a275ca53b5
León Ángel Chancí Guzmán <leonchanci@gmail.com>
2023-07-03 15:50:46 -0500

    Initial commit
    Initial commit

---
+++ src/main/java/com/spring/jpa/pizza/PizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaApplication.class, args);
+	}
+
+}
