1e24393811087988c6b940dc9a46d9982ee4736f
León Ángel Chancí Guzmán <leonchanci@gmail.com>
2023-07-07 22:32:40 -0500

    06_crear_nuevo_registro_crudrepository
    Se crea el método de Guardar Pizza (POST) -> add
    Se crea el método de Actualizar Pizza  (PUT) -> update
    Se crea lógica para validar si el objeto pizza existe o no

--- src/main/java/com/spring/jpa/pizza/persitence/entity/OrderItemId.java
+++ src/main/java/com/spring/jpa/pizza/persitence/entity/OrderItemId.java
@@ -2,9 +2,7 @@ package com.spring.jpa.pizza.persitence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
