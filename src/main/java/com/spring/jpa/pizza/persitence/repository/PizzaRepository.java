3b9d5fbfa733bbb71d32f79885e887883936f53b
León Ángel Chancí Guzmán <leonchanci@gmail.com>
2023-07-09 22:49:02 -0500

    09_query_methods
    Se creand varios QueryMethods con las funciones (True, And, Or, IgnoreCase, Contains y Not)
    se exponen servicios para realizar pruebas

--- src/main/java/com/spring/jpa/pizza/persitence/repository/PizzaRepository.java
+++ src/main/java/com/spring/jpa/pizza/persitence/repository/PizzaRepository.java
@@ -3,6 +3,19 @@ package com.spring.jpa.pizza.persitence.repository;
 import com.spring.jpa.pizza.persitence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
 
+    //QueryMethods-> Obtener todas las pizzas activas ordenadas por precio
+    List<PizzaEntity> findByAvailableTrueOrderByPrice();
+
+    //QueryMethods-> Obtener una pizza activa por el nombre sin importar mayusculas o minusculas
+    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
+
+    //QueryMethods-> Obtener todas las pizzas activas dónde la descripción contenga {description} sin importar mayusculas o minusculas
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String description);
+
+    //QueryMethods-> Obtener todas las pizzas activas dónde la descripción NO contenga {description} sin importar mayusculas o minusculas
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
 }
