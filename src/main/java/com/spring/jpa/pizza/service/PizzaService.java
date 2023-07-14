da8f2ec3f53925ee0d6c2fc943ef35fffe69887c
León Ángel Chancí Guzmán <leonchanci@gmail.com>
2023-07-13 23:35:54 -0500

    12_paginacion_de_consultas
    Se crea servicio para obtener todas las pizzas paginando la información recuperada de BD
    Usando ListPagingAndSortingRepository<>
    
    http://localhost:8090/api/pizzas/pagSort?page=0&elements=6
    
    Se crea servicio para obtener todas todas las Pizzas Activas utilizando paginación y ordenamiento
    http://localhost:8090/api/pizzas/availableWithPagSort?page=0&elements=5&sortBy=name&sortDirection=DESC

--- src/main/java/com/spring/jpa/pizza/service/PizzaService.java
+++ src/main/java/com/spring/jpa/pizza/service/PizzaService.java
@@ -1,35 +1,69 @@
 package com.spring.jpa.pizza.service;
 
 import com.spring.jpa.pizza.persitence.entity.PizzaEntity;
+import com.spring.jpa.pizza.persitence.repository.PizzaPagSortRepository;
 import com.spring.jpa.pizza.persitence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
 public class PizzaService {
     private final PizzaRepository pizzaRepository;
+    private final PizzaPagSortRepository pizzaPagSortRepository;
 
     //Como esta como final se agrega como contructor de parámetros
     @Autowired
-    public PizzaService(PizzaRepository pizzaRepository){
+    public PizzaService(PizzaRepository pizzaRepository, PizzaPagSortRepository pizzaPagSortRepository){
         this.pizzaRepository = pizzaRepository;
+        this.pizzaPagSortRepository = pizzaPagSortRepository;
     }
 
     //--------------GET-------------------//
+
+    /**
+     * Get all pizzas of pizzaRepository
+     */
     public List<PizzaEntity> findAll(){
         //Llamar al jdbcTemplate -> permite crear consultas SQL desde java y convertir
         //El resultado en clases Java
         return this.pizzaRepository.findAll();
     }
 
+    /**
+     * Get all pizzas of pizzaPagSortRepository
+     */
+    public Page<PizzaEntity> findAll(int page, int elements){
+        //Paginar los resultados
+        PageRequest pageRequest = PageRequest.of(page, elements);
+        return this.pizzaPagSortRepository.findAll(pageRequest);
+    }
+
     public PizzaEntity get(int idPizza){
         return this.pizzaRepository.findById(idPizza).orElse(null);
     }
 
+    /**
+     * Get all pizzas available of pizzaRepository
+     */
     public List<PizzaEntity> getAvailable(){
         return this.pizzaRepository.findByAvailableTrueOrderByPrice();
+    }
+
+    /**
+     * Get all pizzas available of pizzaPagSortRepository
+     */
+    public Page<PizzaEntity> getAvailable(int page, int elements, String sortBy, String sortDirection){
+        //sortBy -> Campo por el que se ordenara la consulta
+        //sortDirection -> Forma de ordenación DESC o ASC
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        PageRequest pageRequest = PageRequest.of(page, elements, sort);
+        return this.pizzaPagSortRepository.findByAvailableTrue(pageRequest);
     }
 
     public PizzaEntity getByName(String name){
